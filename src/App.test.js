7141c01da04fa068677d897f3e78c3a3d5d2ade9
Adam Holloway <adam.holloway21@gmail.com>
2020-04-18 15:38:31 +0100

    chore: add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import { App } from './App';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const middlewares = [];
+const mockStore = configureStore(middlewares);
+
+describe('<App />', () => {
+  let store;
+  let component;
+
+  store = mockStore({
+    myState: 'test text',
+  });
+
+  component = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+
+  test('should render the header', () => {
+    const { getByText } = component;
+    const headerText = getByText(/React Redux Rick and Morty API/i);
+    expect(headerText).toBeInTheDocument();
+  });
 });
