aace7a9538d98094dd5d43e22abccd7756df3ccc
Adam Holloway <adam.holloway21@gmail.com>
2020-05-06 22:18:43 +0100

    add another saga and update naming

--- redux-saga/src/App.js
+++ redux-saga/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import {
   HomePage,
@@ -7,37 +6,34 @@ import {
   SingleEpisodePage,
   NotFoundPage,
 } from './pages';
-import { store } from './redux-state/store';
 import './App.css';
 
 export const App = () => {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <div>
-          <header className="App-header">
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/episodes">Episodes</Link>
-                </li>
-              </ul>
-            </nav>
-            <p>React Redux Rick and Morty API</p>
-          </header>
-          <div className="App-content">
-            <Switch>
-              <Route exact path="/" component={HomePage} />
-              <Route exact path="/episodes" component={EpisodesPage} />
-              <Route exact path="/episodes/:id" component={SingleEpisodePage} />
-              <Route path="*" component={NotFoundPage} />
-            </Switch>
-          </div>
+    <BrowserRouter>
+      <div>
+        <header className="App-header">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/episodes">Episodes</Link>
+              </li>
+            </ul>
+          </nav>
+          <p>React Redux Rick and Morty API</p>
+        </header>
+        <div className="App-content">
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/episodes" component={EpisodesPage} />
+            <Route exact path="/episodes/:id" component={SingleEpisodePage} />
+            <Route path="*" component={NotFoundPage} />
+          </Switch>
         </div>
-      </BrowserRouter>
-    </Provider>
+      </div>
+    </BrowserRouter>
   );
 };
