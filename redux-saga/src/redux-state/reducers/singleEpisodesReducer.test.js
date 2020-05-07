9bd8f78c729fb99c4476bda16c33de8f8faddc7f
Adam Holloway <adam.holloway21@gmail.com>
2020-05-07 19:01:50 +0100

    add reducer test for error

--- redux-saga/src/redux-state/reducers/singleEpisodesReducer.test.js
+++ redux-saga/src/redux-state/reducers/singleEpisodesReducer.test.js
@@ -6,9 +6,34 @@ describe('Single episodes reducer', () => {
   it('should start loading a single episode', () => {
     const action = {
       type: actionTypes.LOAD_SINGLE_EPISODE_REQUEST,
+      payload: { id: 1 },
     };
 
     const newState = singleEpisodeReducer({}, action);
     expect(newState.loading).toEqual(true);
+  });
+
+  it('should add a single episode to the store if loading was successful', () => {
+    const action = {
+      type: actionTypes.LOAD_SINGLE_EPISODE_SUCCESS,
+      payload: { episode: mockSingleEpisode },
+    };
+
+    const newState = singleEpisodeReducer({}, action);
+
+    expect(newState.loading).toEqual(false);
+    expect(newState.episode).toEqual(mockSingleEpisode);
+  });
+
+  it('should set error if loading failed', () => {
+    const action = {
+      type: actionTypes.LOAD_SINGLE_EPISODE_FAILURE,
+      payload: { error: new Error() },
+    };
+
+    const newState = singleEpisodeReducer({}, action);
+
+    expect(newState.loading).toEqual(false);
+    expect(newState.error).toEqual(true);
   });
 });
